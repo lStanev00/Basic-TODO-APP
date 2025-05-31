7f2378fffa2cbdc74303e6325b1321a7ef5256ff
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-31 17:39:40 +0300

    basic context with fetches

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import TodoProvider from "./context-providers/context-provider"
 import style from "./styles/App.module.css"
 import './styles/global.css'
 export default function App() {
 
   return (
-    <>
+    <TodoProvider>
+
       <h1 className={style.h1}>Lachezar's TODO List</h1>
-    </>
+
+    </TodoProvider>
+    
   )
 }
---
+++ src/context-providers/context-provider.tsx
@@ -0,0 +1,90 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+interface Item {
+    name: string;
+    task: string;
+    finished: boolean;
+    id?: number;
+}
+
+interface TodoContextType {
+    items: Item[];
+    setItems: React.Dispatch<React.SetStateAction<Item[]>>;
+    addItem: (name: string, task: string, finished: boolean) => void;
+}
+
+const TodoContext = createContext<TodoContextType | null>(null);
+
+const getItems = async (): Promise<Item[]> => {
+    try {
+
+        const res = await fetch("http://localhost:3000/items");
+
+        if (res.status === 200) {
+
+            const data: Item[] = await res.json();
+
+
+            return data;
+
+        }
+    } catch (error) {
+        console.warn(error);
+    }
+    return [];
+};
+
+const postItem = async (item: Item): Promise<Item|null>  => {
+    try {
+
+        const res = await fetch("http://localhost:3000/items", {
+            headers: {
+                "content-type": "application-json"
+            },
+            method: "POST",
+            body: JSON.stringify({
+                name: item.name,
+                task: item.task,
+                finished: item.finished
+            })
+        });
+
+        if (res.status === 200) {
+
+            const data: Item = await res.json();
+
+            return data;
+
+        }
+    } catch (error) {
+        console.warn(error);
+    }
+    return null;
+};
+
+
+export default function TodoProvider({ children }: { children: ReactNode }) {
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const data = await getItems();
+            setItems(data);
+        })();
+    }, []);
+
+    const addItem = (name: string, task: string, finished: boolean) => {
+        const newItem: Item = {
+            name: name,
+            task: task,
+            finished: finished,
+        };
+        setItems((prev) => [...prev, newItem]);
+    };
+
+    return (
+        <TodoContext.Provider value={{ items, setItems, addItem }}>
+            {children}
+        </TodoContext.Provider>
+    );
+}
